fa344b761510616503293ca3ed3c8b0716efaff9
alegutb <134803966+alegutb@users.noreply.github.com>
2023-08-26 12:11:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
+// Generate a random algebraic equation for the game
 function generateEquation() {
-  // Generate different equation types with varying coefficients, constants, and results
-  const equationTypes = [
-    { equation: `2x + 5 = 15`, coefficient: 2, constant: 5, result: 15 },
-    { equation: `4x - 10 = 18`, coefficient: 4, constant: -10, result: 18 },
-    { equation: `5x + 3 = 23`, coefficient: 5, constant: 3, result: 23 }
-    // Add more equation types here
-  ];
+  const coefficient = Math.floor(Math.random() * 10) + 1; // Random coefficient between 1 and 10
+  const constant = Math.floor(Math.random() * 20) + 1;    // Random constant between 1 and 20
+  const result = Math.floor(Math.random() * 50) + 1;      // Random result between 1 and 50
 
-  const randomIndex = Math.floor(Math.random() * equationTypes.length);
-  const selectedEquation = equationTypes[randomIndex];
-
-  return {
-    equation: selectedEquation.equation,
-    coefficient: selectedEquation.coefficient,
-    xValue: (selectedEquation.result - selectedEquation.constant) / selectedEquation.coefficient
-  };
+  const equation = `${coefficient}x + ${constant} = ${result}`;
+  return { equation, xValue: (result - constant) / coefficient };
 }
 
 const questionElement = document.getElementById('question');
 const answerElement = document.getElementById('answer');
 const resultElement = document.getElementById('result');
 const checkButton = document.getElementById('checkButton');
-const nextButton = document.getElementById('nextButton');
+
+let currentEquation = generateEquation();
+questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
+
+checkButton.addEventListener('click', () => {
+  const userAnswer = parseFloat(answerElement.value);
+  if (!isNaN(userAnswer)) {
+    if (userAnswer === currentEquation.xValue) {
+      resultElement.textContent = 'Correct! Well done.';
+    } else {
+      resultElement.textContent = 'Incorrect. Try again.';
+    }
+
+    // Generate a new equation for the next round
+    currentEquation = generateEquation();
+    questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
+    answerElement.value = '';
+  } else {
+    resultElement.textContent = 'Please enter a valid number.';
+  }
+});
+// ... (previous code)
+
+const scoreElement = document.getElementById('score');
+@@ -39,3 +74,50 @@ nextButton.addEventListener('click', () => {
+function updateScore() {
+  scoreElement.textContent = score;
+}
+// ... (previous code)
+
 const hintButton = document.getElementById('hintButton');
 const skipButton = document.getElementById('skipButton');
 const timerElement = document.getElementById('timer');
-const scoreElement = document.getElementById('score');
-
-let currentEquation;
-let score = 0;
-let time = 60;
+let time = 60; // 60 seconds countdown
 let timerInterval;
+
+hintButton.addEventListener('click', () => {
+  const hint = `Divide both sides by ${currentEquation.coefficient} to solve for x.`;
+  alert(hint);
+});
+
+skipButton.addEventListener('click', () => {
+  currentEquation = generateEquation();
+  questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
+  answerElement.value = '';
+  resultElement.textContent = '';
+  answerElement.disabled = false;
+  checkButton.disabled = false;
+  nextButton.style.display = 'none';
+  hintButton.style.display = 'none';
+  skipButton.style.display = 'none';
+});
 
 function startTimer() {
   timerInterval = setInterval(() => {
@@ -53,63 +86,6 @@ function gameOver() {
   skipButton.style.display = 'none';
 }
 
-function updateScore() {
-  scoreElement.textContent = score;
-}
-
-checkButton.addEventListener('click', () => {
-  const userAnswer = parseFloat(answerElement.value);
-  if (!isNaN(userAnswer)) {
-    if (userAnswer === currentEquation.xValue) {
-      resultElement.textContent = 'Correct! Well done.';
-      score += 10;
-    } else {
-      resultElement.textContent = `Incorrect. The correct answer is ${currentEquation.xValue}.`;
-    }
-    
-    answerElement.disabled = true;
-    checkButton.disabled = true;
-    nextButton.style.display = 'block';
-    hintButton.style.display = 'none';
-    skipButton.style.display = 'none';
-
-    updateScore();
-  } else {
-    resultElement.textContent = 'Please enter a valid number.';
-  }
-});
-
-nextButton.addEventListener('click', () => {
-  currentEquation = generateEquation();
-  questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
-  answerElement.value = '';
-  resultElement.textContent = '';
-  answerElement.disabled = false;
-  checkButton.disabled = false;
-  nextButton.style.display = 'none';
-  hintButton.style.display = 'block';
-  skipButton.style.display = 'block';
-});
-
-hintButton.addEventListener('click', () => {
-  const hint = `Divide both sides by ${currentEquation.coefficient} to solve for x.`;
-  alert(hint);
-});
-
-skipButton.addEventListener('click', () => {
-  currentEquation = generateEquation();
-  questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
-  answerElement.value = '';
-  resultElement.textContent = '';
-  answerElement.disabled = false;
-  checkButton.disabled = false;
-  nextButton.style.display = 'none';
-  hintButton.style.display = 'block';
-  skipButton.style.display = 'block';
-});
-
 startTimer();
-currentEquation = generateEquation();
-questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
 
 
