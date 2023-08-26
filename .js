ddc72b91286b3c1dd7fce1c3ebd4bb1ba5de57e6
alegutb <134803966+alegutb@users.noreply.github.com>
2023-08-25 20:22:53 -0500

    Update .js

--- .js
+++ .js
@@ -1,30 +1,57 @@
-const equationElement = document.getElementById('equation');
-const inputElement = document.getElementById('input');
-const checkBtn = document.getElementById('checkBtn');
+const problemElement = document.getElementById("problem");
+const userAnswerInput = document.getElementById("userAnswer");
+const submitBtn = document.getElementById("submitBtn");
+const resultElement = document.getElementById("result");
+const timeElement = document.getElementById("time");
 
-function generateEquation() {
-  const operators = ['+', '-', '*', '/'];
-  const num1 = Math.floor(Math.random() * 20) + 1;
-  const num2 = Math.floor(Math.random() * 20) + 1;
-  const operator = operators[Math.floor(Math.random() * operators.length)];
-  const equation = `${num1} ${operator} ${num2}`;
-  equationElement.textContent = equation;
+let timer;
+let timeLeft = 60;
+
+function generateProblem() {
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+    const operators = ["+", "-", "*", "/"];
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+    
+    problemElement.textContent = `${num1} ${operator} ${num2}`;
+    
+    return eval(`${num1} ${operator} ${num2}`);
 }
 
-checkBtn.addEventListener('click', () => {
-  const userAnswer = inputElement.value;
-  const equation = equationElement.textContent;
-  const correctAnswer = eval(equation).toString();
-  
-  if (userAnswer === correctAnswer) {
-    alert('Correct! Well done.');
-  } else {
-    alert('Incorrect. Try again.');
-  }
-  
-  generateEquation();
-  inputElement.value = '';
+function startTimer() {
+    timer = setInterval(function() {
+        timeLeft--;
+        timeElement.textContent = `${timeLeft} seconds`;
+
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            submitBtn.disabled = true;
+            resultElement.textContent = "Time's up!";
+        }
+    }, 1000);
+}
+
+function checkAnswer() {
+    const correctAnswer = generateProblem();
+    const userAnswer = parseFloat(userAnswerInput.value);
+    
+    if (userAnswer === correctAnswer) {
+        resultElement.textContent = "Correct!";
+    } else {
+        resultElement.textContent = "Incorrect!";
+    }
+    
+    userAnswerInput.value = "";
+}
+
+submitBtn.addEventListener("click", function() {
+    checkAnswer();
 });
 
-// Initial setup
-generateEquation();
+userAnswerInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+});
+
+startTimer();
