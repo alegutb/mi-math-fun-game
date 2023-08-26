f0db987b0c1978dc451c2db8f3a646899cc3be74
alegutb <134803966+alegutb@users.noreply.github.com>
2023-08-26 12:17:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,149 @@
-// Generate a random algebraic equation for the game
-function generateEquation() {
-  const coefficient = Math.floor(Math.random() * 10) + 1; // Random coefficient between 1 and 10
-  const constant = Math.floor(Math.random() * 20) + 1;    // Random constant between 1 and 20
-  const result = Math.floor(Math.random() * 50) + 1;      // Random result between 1 and 50
+const equationElement = document.getElementById("equation");
+const answerElement = document.getElementById("answer");
+const resultElement = document.getElementById("result");
+const scoreElement = document.getElementById("score");
+const attemptsElement = document.getElementById("attempts");
+const timerElement = document.getElementById("timer");
+const gameContainer = document.getElementById("game-container");
+const gameOverContainer = document.getElementById("game-over-container");
+const restartButton = document.getElementById("restartButton");
+const hintButton = document.getElementById("hintButton");
+const difficultySelect = document.getElementById("difficultySelect");
+const startGameButton = document.getElementById("startGameButton");
+const startTimerButton = document.getElementById("startTimerButton");
 
-  const equation = `${coefficient}x + ${constant} = ${result}`;
-  return { equation, xValue: (result - constant) / coefficient };
-}
+const difficulties = [
+    { name: "Easy", equations: [
+        { equation: "2x + 5 = 15", answer: 5 },
+        { equation: "3y - 7 = 10", answer: 17/3 },
+        { equation: "4z + 8 = 28", answer: 5 },
+    ]},
+    { name: "Medium", equations: [
+        { equation: "5a - 12 = 33", answer: 9 },
+        { equation: "6b + 4 = 22", answer: 3 },
+        { equation: "9c - 15 = 51", answer: 8 },
+        { equation: "2d + 3 = 5d - 1", answer: 2 },
+        { equation: "4e + 7 = 2e + 18", answer: 5 },
+    ]},
+    { name: "Hard", equations: [
+        { equation: "2x + 3 = 5x - 1", answer: 2 },
+        { equation: "4y + 7 = 2y + 18", answer: 5 },
+        { equation: "3z + 9 = 2z + 21", answer: 12 },
+        { equation: "7a - 4 = 3a + 12", answer: 8 },
+        { equation: "6b + 5 = 8b - 3", answer: -4 },
+    ]},
+];
 
-const questionElement = document.getElementById('question');
-const answerElement = document.getElementById('answer');
-const resultElement = document.getElementById('result');
-const checkButton = document.getElementById('checkButton');
-
-let currentEquation = generateEquation();
-questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
-
-checkButton.addEventListener('click', () => {
-  const userAnswer = parseFloat(answerElement.value);
-  if (!isNaN(userAnswer)) {
-    if (userAnswer === currentEquation.xValue) {
-      resultElement.textContent = 'Correct! Well done.';
-    } else {
-      resultElement.textContent = 'Incorrect. Try again.';
-    }
-
-    // Generate a new equation for the next round
-    currentEquation = generateEquation();
-    questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
-    answerElement.value = '';
-  } else {
-    resultElement.textContent = 'Please enter a valid number.';
-  }
-});
-// ... (previous code)
-
-const scoreElement = document.getElementById('score');
-@@ -39,3 +74,50 @@ nextButton.addEventListener('click', () => {
-function updateScore() {
-  scoreElement.textContent = score;
-}
-// ... (previous code)
-
-const hintButton = document.getElementById('hintButton');
-const skipButton = document.getElementById('skipButton');
-const timerElement = document.getElementById('timer');
-let time = 60; // 60 seconds countdown
+const maxAttempts = 3;
+const gameTimeInSeconds = 60;
+let currentEquationIndex = 0;
+let score = 0;
+let attempts = maxAttempts;
 let timerInterval;
+let currentDifficultyIndex = 0;
+let isGameStarted = false;
+let isTimerStarted = false;
 
-hintButton.addEventListener('click', () => {
-  const hint = `Divide both sides by ${currentEquation.coefficient} to solve for x.`;
-  alert(hint);
-});
+function displayEquation() {
+    equationElement.textContent = difficulties[currentDifficultyIndex].equations[currentEquationIndex].equation;
+    resultElement.textContent = "";
+    answerElement.value = "";
+    scoreElement.textContent = `Score: ${score}`;
+    attemptsElement.textContent = `Attempts left: ${attempts}`;
+}
 
-skipButton.addEventListener('click', () => {
-  currentEquation = generateEquation();
-  questionElement.textContent = `Solve for x: ${currentEquation.equation}`;
-  answerElement.value = '';
-  resultElement.textContent = '';
-  answerElement.disabled = false;
-  checkButton.disabled = false;
-  nextButton.style.display = 'none';
-  hintButton.style.display = 'none';
-  skipButton.style.display = 'none';
-});
+function checkAnswer() {
+    const userAnswer = parseFloat(answerElement.value);
+    const correctAnswer = difficulties[currentDifficultyIndex].equations[currentEquationIndex].answer;
+
+    if (userAnswer === correctAnswer) {
+        resultElement.textContent = "Correct!";
+        score++;
+        currentEquationIndex = (currentEquationIndex + 1) % difficulties[currentDifficultyIndex].equations.length;
+        if (score === difficulties[currentDifficultyIndex].equations.length) {
+            showGameOver(true);
+            return;
+        }
+        displayEquation();
+    } else {
+        resultElement.textContent = "Incorrect. Try again.";
+        attempts--;
+        attemptsElement.textContent = `Attempts left: ${attempts}`;
+        if (attempts === 0) {
+            showGameOver(false);
+        }
+    }
+}
+
+function startGame() {
+    difficultySelect.disabled = true;
+    displayEquation();
+}
 
 function startTimer() {
-  timerInterval = setInterval(() => {
-    if (time > 0) {
-      time--;
-      timerElement.textContent = `Time: ${time} s`;
+    startTimerButton.disabled = true;
+    clearInterval(timerInterval);
+    let timeLeft = gameTimeInSeconds;
+    timerInterval = setInterval(() => {
+        timerElement.textContent = `Time left: ${timeLeft} s`;
+        timeLeft--;
+        if (timeLeft < 0) {
+            clearInterval(timerInterval);
+            showGameOver(false);
+        }
+    }, 1000);
+}
+
+function changeDifficulty() {
+    currentDifficultyIndex = (currentDifficultyIndex + 1) % difficulties.length;
+    currentEquationIndex = 0;
+    score = 0;
+    attempts = maxAttempts;
+    clearInterval(timerInterval);
+    startTimerButton.disabled = false;
+    startTimerButton.textContent = "Start Timer";
+    startGame();
+}
+
+function showGameOver(isWinner) {
+    clearInterval(timerInterval);
+    gameContainer.style.display = "none";
+    gameOverContainer.style.display = "block";
+    if (isWinner) {
+        gameOverContainer.innerHTML = `<h2>Congratulations!</h2><p>You completed all equations with a score of ${score}.</p>`;
     } else {
-      clearInterval(timerInterval);
-      gameOver();
+        gameOverContainer.innerHTML = `<h2>Game Over</h2><p>Your final score is ${score}. Try again!</p>`;
     }
-  }, 1000);
 }
 
-function gameOver() {
-  questionElement.textContent = 'Time\'s up! Game over.';
-  answerElement.disabled = true;
-  checkButton.disabled = true;
-  nextButton.style.display = 'none';
-  hintButton.style.display = 'none';
-  skipButton.style.display = 'none';
-}
+hintButton.addEventListener("click", () => {
+    const correctAnswer = difficulties[currentDifficultyIndex].equations[currentEquationIndex].answer;
+    resultElement.textContent = `The answer is ${correctAnswer}.`;
+});
 
-startTimer();
+startGameButton.addEventListener("click", () => {
+    if (!isGameStarted) {
+        isGameStarted = true;
+        startGameButton.textContent = "Game Started";
+        startGame();
+    }
+});
+
+startTimerButton.addEventListener("click", () => {
+    if (!isTimerStarted) {
+        isTimerStarted = true;
+        startTimerButton.textContent = "Timer Started";
+        startTimer();
+    }
+});
+
+difficultySelect.addEventListener("change", changeDifficulty);
+restartButton.addEventListener("click", () => {
+    location.reload();
+});
+
+// Initialize the game
+displayEquation();
+
 
 
